eb0dc7b63ef10c4f55745c4ca74bb68448987094
Aryan20apr <aryansingh20nov@gmail.com>
2024-09-26 14:44:59 +0530

    Create Incident: Create incident endpoint

--- src/main/java/com/aryansingh/securityincident/controllers/IncidentController.java
+++ src/main/java/com/aryansingh/securityincident/controllers/IncidentController.java
@@ -1,13 +1,30 @@
 package com.aryansingh.securityincident.controllers;
 
+import com.aryansingh.securityincident.models.dtos.IncidentDTO;
+import com.aryansingh.securityincident.models.entities.Incident;
+import com.aryansingh.securityincident.services.IncidentService;
+import com.aryansingh.securityincident.utils.ApiResponse;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/incident/api/v1")
+@AllArgsConstructor
 public class IncidentController {
 
+    IncidentService incidentService;
 
+    @PostMapping("/new")
+    public ResponseEntity<ApiResponse<String>> newIncident(@Valid @RequestBody IncidentDTO incident) {
+
+        String token = incidentService.createIncident(incident);
+
+        return new ResponseEntity<>(new ApiResponse<>("Incident reported successfully",token), HttpStatus.CREATED);
+    }
 }
---
+++ src/main/java/com/aryansingh/securityincident/utils/AppConstants.java
@@ -0,0 +1,9 @@
+package com.aryansingh.securityincident.utils;
+
+public class AppConstants {
+
+    public static final String SUCCESS_MESSAGE ="Success !";
+
+    public static final String ERROR_MESSAGE ="Error !!";
+
+}
